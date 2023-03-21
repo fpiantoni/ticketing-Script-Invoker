463eb52a373e4489936e9d04b99763cbc0f846d0
fpiantoni <fran@piantoni.com.ar>
2023-03-21 15:16:23 -0300

    added: cron Job for terminating job in case of success

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
-var childProc = require('child_process');
-const axios = require('axios');
+const childProc = require('child_process')
+const cron = require('node-cron')
+const axios = require('axios')
 
 const requestWebsiteData = async () => {
+    // console.log('Verifying website data...')
     try {
-        let request = await axios.get('https://deportick.queue-it.net/?c=deportick&e=argentinacurazao');
-        if (request.request._redirectable._options.path === "/error?er=2&cid=en-US") {
-            console.log("Page NOT available")
+        let request = await axios.get(
+            'https://deportick.queue-it.net/?c=deportick&e=argentinacurazao'
+        )
+        if (
+            request.request._redirectable._options.path ===
+            '/error?er=2&cid=en-US'
+        ) {
+            console.log('Page NOT available')
+            return true
+        } else {
+            console.log('Page is AVAILABLE')
+            for (i = 0; i < 25; i++) {
+                childProc.exec(
+                    'open -a "Google Chrome" "https://deportick.queue-it.net/?c=deportick&e=argentinacurazao"'
+                )
+            }
+            return false
         }
     } catch (err) {
-        console.log(err);
+        console.log(err)
     }
-   
-    
 }
 
-requestWebsiteData()
+let CronJob = cron.schedule('*/10 * * * * *', async () => {
+    let script = await requestWebsiteData()
+    if (script === false) console.log('Terminating Job'), CronJob.stop()
+})
 
-// for (i=0; i<10; i++) {
-
-// childProc.exec('open -a "Google Chrome" https://bircle.io');
-
-// }
-
+CronJob.start()
