85a70b9037f8e7f266a16d2d417a3ec6fcf7781d
fpiantoni <fran@piantoni.com.ar>
2023-03-22 12:13:13 -0300

    imporved: chrome launcher with cognito mode without website data

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
 const childProc = require('child_process')
+const chromeLauncher = require('chrome-launcher')
 const cron = require('node-cron')
 const axios = require('axios')
+
+//Please notice that you don't have to kill the terminal process becuasue it will close
+// the chrome applications.
+
+const launchChrome = async () => {
+    const baseUrl =
+        'https://deportick.queue-it.net/?c=deportick&e=argentinacurazao'
+    const chromeFlags = [
+        // '--disable-extensions',
+        // '--disable-plugins',
+        '--disable-sync',
+        '--disable-local-storage',
+        '--disable-session-storage',
+        '--disable-application-cache',
+        '--disable-cache',
+        '--no-first-run',
+        '--no-default-browser-check',
+        // '--disable-infobars',
+        '--incognito',
+        baseUrl,
+    ]
+
+    const chromeOptions = {
+        chromeFlags: chromeFlags,
+    }
+
+    let amountToLaunch = 20
+    for (let i = 0; i < amountToLaunch; i++) {
+        const chrome = await chromeLauncher.launch(chromeOptions)
+        console.log(`Chrome debugging port running on ${chrome.port}`)
+    }
+
+    // Prevent SIGINT from terminating the Chrome process
+    // process.on('SIGINT', () => {
+    //     console.log('Caught interrupt signal')
+    //     chrome.kill()
+    //     process.exit()
+    // })
+}
 
 const requestWebsiteData = async () => {
     // console.log('Verifying website data...')
@@ -8,25 +48,24 @@ const requestWebsiteData = async () => {
         let request = await axios.get(
             'https://deportick.queue-it.net/?c=deportick&e=argentinacurazao'
         )
+        console.log(request.request._redirectable._options.path)
         if (
-            request.request._redirectable._options.path ===
+            request.request._redirectable._options.path !=
             '/error?er=2&cid=en-US'
         ) {
             console.log('Page NOT available')
             return true
         } else {
             console.log('Page is AVAILABLE')
-            for (i = 0; i < 10; i++) {
-                childProc.exec(
-                    'open -a "Google Chrome" "https://deportick.queue-it.net/?c=deportick&e=argentinacurazao"'
-                )
-                childProc.exec(
-                    'open -a "Brave Browser" "https://deportick.queue-it.net/?c=deportick&e=argentinacurazao"'
-                )
-                childProc.exec(
-                    'open -a "Safari" "https://deportick.queue-it.net/?c=deportick&e=argentinacurazao"'
-                )
-            }
+
+            launchChrome()
+            childProc.exec(
+                'open -a "Brave Browser" "https://deportick.queue-it.net/?c=deportick&e=argentinacurazao"'
+            )
+            childProc.exec(
+                'open -a "Safari" "https://deportick.queue-it.net/?c=deportick&e=argentinacurazao"'
+            )
+
             return false
         }
     } catch (err) {
